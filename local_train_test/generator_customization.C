15143aa1aacd23fbabd262b4d36fafc17a0b7e3b
lmichele <luca.micheletti@cern.ch>
2020-03-12 09:57:10 +0100

    Trying to add more events in the file...

--- local_train_test/generator_customization.C
+++ local_train_test/generator_customization.C
@@ -1,27 +1,24 @@
-Bool_t myTrigger(AliStack *stack)
+Bool_t userTrigger(AliStack *stack)
 {
-	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
-	printf("TRIGGER SETTATO ESTERNAMENTE \n");
+	printf("____________________________\n");
+	printf("USER TRIGGER IMPLEMENTATION \n");
 	Int_t nTracks  = stack->GetNtrack();
   printf("n Tracks = %i \n",nTracks);
+	printf("____________________________\n");
   if(nTracks>100){return kTRUE;}
-  else{
-    printf("n Tracks < %i --> EVENT DISCARDED \n",100);
-    return kFALSE;
-  }
-	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+  else{return kFALSE;}
 }
 
 void generator_customization(AliGenerator* generator)
 {
 	/// myCode
-  //gROOT->LoadMacro("/home/luca/GITHUB/service_task/local_train_test/myTrigger.C+");
+  //gROOT->LoadMacro("/home/luca/GITHUB/service_task/local_train_test/userTrigger.C+");
   /// myCode
 
 	((AliGenExtExec*) generator)->SetPathScript("$ALICE_PHYSICS/PWG/MCLEGO/JEWEL/gen_jewel.sh");
 	//((AliGenExtFile*) generator)->SetMultiplicityTrigger(100);
 	//((AliGenExtFile*) generator)->SetPtTrigger(5.);
 
-	std::function<Bool_t(AliStack*)> funcMyTrigger = myTrigger;
-	((AliGenExtFile*) generator)->SetUserTrigger(funcMyTrigger);
+	std::function<Bool_t(AliStack*)> funcUserTrigger = userTrigger;
+	((AliGenExtFile*) generator)->SetUserTrigger(funcUserTrigger);
 }
