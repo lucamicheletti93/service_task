338b9125c61e0dfe337e4055a0ed5eaf13a5b2c1
lmichele <luca.micheletti@cern.ch>
2020-07-01 10:09:32 +0200

    Adding the user trigger implementation for EPOS LHC

--- external_generators/EPOS_LHC/generator_customization.C
+++ external_generators/EPOS_LHC/generator_customization.C
@@ -6,10 +6,14 @@ Bool_t userTrigger(AliStack *stack)
 	Int_t nTracks  = stack->GetNtrack();
   	printf("n Tracks = %i \n",nTracks);
 	printf("____________________________\n");
-  	if(nTracks>0){return kTRUE;}
+  	if(nTracks>50){return kTRUE;}
   	else{return kFALSE;}
 }
 
 void generator_customization(AliGenerator* generator){
 	((AliGenExtExec*) generator)->SetPathScript("$ALICE_PHYSICS/PWG/MCLEGO/CRMC/gen_eposlhc.sh");
+	// Trigger : function ptr
+  	Bool_t (*funcUserTrigger)(AliStack*) = userTrigger;
+  	generator->SetUserTrigger(funcUserTrigger);
+
 }
